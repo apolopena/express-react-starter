e5f9b9f325e1964f7d5ce22b05f839d2d73cc2a9
apolopena <3060702+apolopena@users.noreply.github.com>
2021-07-27 10:23:02 -0700

    ➕ ADD: bugfix and error handling

--- server/src/routes/math.js
+++ server/src/routes/math.js
@@ -7,9 +7,31 @@ function getMathRoutes() {
   return router
 }
 
-async function add(req, res) {
-  const sum = Number(req.query.a) + Number(req.query.c)
-  res.send(sum.toString())
+async function add(req, res, next) {
+  const required = ['a', 'b']
+  const missing = required.reduce((acc, val) => {
+    if (!req.query.hasOwnProperty(val)) acc.push(val)
+    return acc
+  }, [])
+ if (missing.length > 0) {
+   return res.status(400)
+    .send({ code: 400, error: `missing parameter${missing.length > 1 ? 's' : ''} ${missing}` })
+ }
+  const num = {
+    a: Number(req.query.a),
+    b: Number(req.query.b)
+  }
+  if (isNaN(num.a)) {
+    return res.status(400)
+    .send({ code: 400, error: `parameter a: ${req.query.a} is not a number` })
+  }
+  if (isNaN(num.b)) {
+    return res.status(400)
+    .send({ code: 400, error: `parameter b: ${req.query.b} is not a number` })
+  }
+  res.json({
+    sum: (num.a + num.b).toString()
+  })
 }
 
 async function subtract(req, res) {
