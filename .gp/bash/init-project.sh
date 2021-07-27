17a824f09153fcdedbaa60eab392d6e6ac0b4dcd
apolopena <3060702+apolopena@users.noreply.github.com>
2021-07-27 10:18:04 -0700

    ➕ ADD: setup mysql user

--- .gp/bash/init-project.sh
+++ .gp/bash/init-project.sh
@@ -6,7 +6,25 @@
 
 # Load logger
 . .gp/bash/workspace-init-logger.sh
+# Load spinner
+ . .gp/bash/spinner.sh
 
+# create hello_world database
+db_name=hello_world
+db_exists=$(mysqlshow  2>/dev/null | grep "$db_name" >/dev/null 2>&1 && echo "1" || echo "0")
+if [[ $db_exists == 0 ]]; then
+  msg="Creating database: $db_name"
+  log_silent "$msg" && start_spinner "$msg"
+  mysql -e "CREATE DATABASE $db_name;"
+  err_code=$?
+  if [ $err_code != 0 ]; then
+    stop_spinner $err_code
+    log -e "ERROR: $msg"
+  else
+    stop_spinner $err_code
+    log_silent "SUCCESS: $msg"
+  fi
+fi
 # BEGIN example code block - migrate database
 # . .gp/bash/spinner.sh # COMMENT: Load spinner
 # __migrate_msg="Migrating database"
