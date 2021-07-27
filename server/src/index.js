4902fbaeb2bb548e719c53a86c7c16b25751481c
apolopena <3060702+apolopena@users.noreply.github.com>
2021-07-27 10:36:41 -0700

    🐛 FIX: imports

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,5 @@
 import logger from 'loglevel'
-import {port} from './config'
-import {startServer} from './start'
+import { startServer } from './start'
 
 const isTest = process.env.NODE_ENV === 'test'
 const logLevel = process.env.LOG_LEVEL || (isTest ? 'warn' : 'info')
