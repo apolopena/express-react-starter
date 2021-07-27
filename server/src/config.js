7d3b5a361cf1d17013b819d2e77db383633e5d1a
apolopena <3060702+apolopena@users.noreply.github.com>
2021-07-27 10:24:49 -0700

    🐛 FIX: defaults

--- server/src/config.js
+++ server/src/config.js
@@ -1,6 +1,17 @@
 // preload env vars when in development, run: node -r dotenv/config index.js 
-const dotenv = require('dotenv');
+import { exec } from 'child_process'
+import dotenv from 'dotenv';
 dotenv.config();
 module.exports = {
-  port: process.env.PORT
+  port: process.env.PORT,
+  mySqlUser: process.env.MYSQL_USER,
+  mySqlPass: process.env.MYSQL_PW,
+  mySqlHost: process.env.MYSQL_HOST || exec("gp url", (err, stdout, stderr) => {
+    if (err) {
+      console.log("error executing gp")
+      return ""
+    }
+    return stdout
+  }),
+  mySqlDatabase: process.env.MYSQL_DB
 };
