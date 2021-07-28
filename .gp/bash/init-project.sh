8ded439f5e7594ec7ebdf7ce157fab87614d6a23
apolopena <penavilla@gmail.com>
2021-07-28 18:25:48 +0000

    🐛 FIX: create test database

--- .gp/bash/init-project.sh
+++ .gp/bash/init-project.sh
@@ -39,6 +39,23 @@ install_deps() {
   return 0
 }
 
+# create hello_world database
+db_name=hello_world
+db_exists=$(mysqlshow  2>/dev/null | grep "$db_name" >/dev/null 2>&1 && echo "1" || echo "0")
+if [[ $db_exists == 0 ]]; then
+  msg="Creating database: $db_name"
+  log_silent "$msg" && start_spinner "$msg"
+  mysql -e "CREATE DATABASE $db_name;"
+  err_code=$?
+  if [ $err_code != 0 ]; then
+    stop_spinner $err_code
+    log -e "ERROR: $msg"
+  else
+    stop_spinner $err_code
+    log_silent "SUCCESS: $msg"
+  fi
+fi
+
 # Init server
 install_deps "server"
 # Init client
