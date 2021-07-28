6582f363dd197ba1fc44b961608e3e54278e4674
apolopena <penavilla@gmail.com>
2021-07-28 18:16:52 +0000

    🐛 FIX: make defaults strings

--- server/src/config.js
+++ server/src/config.js
@@ -4,8 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 module.exports = {
   port: process.env.PORT || 7878,
-  mySqlUser: process.env.MYSQL_USER || pmasu,
-  mySqlPass: process.env.MYSQL_PW || 123456,
+  mySqlUser: process.env.MYSQL_USER || 'pmasu',
+  mySqlPass: process.env.MYSQL_PW || '123456',
   mySqlHost: process.env.MYSQL_HOST || 'localhost',
   mySqlDatabase: process.env.MYSQL_DB || 'hello_world'
 };
