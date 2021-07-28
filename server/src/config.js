4b5857a36f4fe10bc7a5b6be9c03ec53f78ac71c
apolopena <penavilla@gmail.com>
2021-07-28 18:09:28 +0000

    🐛 FIX: defaults so tests can work on gitpod by default

--- server/src/config.js
+++ server/src/config.js
@@ -3,15 +3,9 @@ import { exec } from 'child_process'
 import dotenv from 'dotenv';
 dotenv.config();
 module.exports = {
-  port: process.env.PORT,
-  mySqlUser: process.env.MYSQL_USER,
-  mySqlPass: process.env.MYSQL_PW,
-  mySqlHost: process.env.MYSQL_HOST || exec("gp url", (err, stdout, stderr) => {
-    if (err) {
-      console.log("error executing gp")
-      return ""
-    }
-    return stdout
-  }),
-  mySqlDatabase: process.env.MYSQL_DB
+  port: process.env.PORT || 7878,
+  mySqlUser: process.env.MYSQL_USER || pmasu,
+  mySqlPass: process.env.MYSQL_PW || 123456,
+  mySqlHost: process.env.MYSQL_HOST || 'localhost',
+  mySqlDatabase: process.env.MYSQL_DB || 'hello_world'
 };
