5f406553fa1b4a4cef9627acb9572682c493c5aa
apolopena <3060702+apolopena@users.noreply.github.com>
2021-08-04 19:08:49 -0700

    ♻️ REFACTOR: pool vars and fallbacks

--- server/src/config.js
+++ server/src/config.js
@@ -2,10 +2,31 @@
 import { exec } from 'child_process'
 import dotenv from 'dotenv';
 dotenv.config();
-module.exports = {
+
+const fallback = {
+  user: 'pmasu',
+  pass: '123456',
+  host: 'localhost',
+  database: 'hello_world'
+}
+const cfg = {
   port: process.env.PORT || 7878,
-  mySqlUser: process.env.MYSQL_USER || 'pmasu',
-  mySqlPass: process.env.MYSQL_PW || '123456',
-  mySqlHost: process.env.MYSQL_HOST || 'localhost',
-  mySqlDatabase: process.env.MYSQL_DB || 'hello_world'
+  mySqlUser: process.env.MYSQL_USER || fallback.user,
+  mySqlPass: process.env.MYSQL_PW || fallback.pass,
+  mySqlHost: process.env.MYSQL_HOST || fallback.host,
+  mySqlDatabase: process.env.MYSQL_DB || fallback.database,
+}
+module.exports = {
+  port: cfg.port,
+  mySqlUser: cfg.mySqlUser,
+  mySqlPass: cfg.mySqlPass,
+  mySqlHost: cfg.mySqlHost,
+  mySqlDatabase: cfg.mySqlDatabase,
+  mySqlPoolConfig: {
+    connectionLimit: 150,
+    host: process.env.MYSQL_POOL_HOST || cfg.mySqlHost,
+    user: process.env.MYSQL_POOL_USER || cfg.mySqlUser,
+    password: process.env.MYSQL_POOL_PW || cfg.mySqlPass,
+    database: process.env.MYSQL_POOL_DB || cfg.mySqlDatabase
+  }
 };
